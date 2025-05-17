6a9067d20fd66b5e19a35525dad06705eac059ee
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-05-17 16:39:52 +0700

    create new file cpp

--- soalGanjil.cpp
+++ soalGanjil.cpp
@@ -5,20 +5,35 @@ using namespace std;
 class mahasiswa{
     public:
         string nama, nim;
+
     private:
         int nilai;
+
     friend class dosen;
 };
 
 class dosen{
     public:
         string nama, NDIN;
+
     private:
         string pangkat, gaji;
 
     friend class staff;
+    friend void univ::gajiDosen(dosen &d);
 };
 
-int main(){
-    
+class staff{
+    public:
+        string nama, idStaff;
+    private:
+        string gaji;
+};
+
+class univ{
+    public:
+        void gajiDosen(dosen &d);
+};
+
+int main(){;
 }
