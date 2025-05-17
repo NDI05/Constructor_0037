1bd889eae48923d4fb1fd11e7d2667463ca21e21
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-05-17 16:41:31 +0700

    create class and chain function

--- chainFunction.cpp
+++ chainFunction.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class buku{
+    string judul;
+    public:
+        buku setJudul(string judul){
+            this->judul = judul;
+            return *this;
+        }
+};
