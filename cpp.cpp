d43e54737c43648b61b33a25fafe62857adc3685
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-05-19 10:47:26 +0700

    done analyze constructor and destructor

--- cpp.cpp
+++ cpp.cpp
@@ -2,9 +2,41 @@
 using namespace std;
 
 class mahasiswa{
+    private:
+        static int jumlahMhs;
+    public:
+        string nama;
+        mahasiswa(string  pNama){
+            nama = pNama;
+            ++jumlahMhs;
+            cout << "Mahasiswa di buat " << nama << endl;
+        };
+        ~mahasiswa(){
+            --jumlahMhs;
+            cout << "Nama Terhapus " << nama << endl;
+        };
 
+        static void setJumlah(int pJumlah){
+            jumlahMhs = pJumlah;
+        }
+        static int getJumlahMahasiswa(){
+            return jumlahMhs;
+        }
+    
 };
 
-int main(){
+int mahasiswa::jumlahMhs = 0;
 
+int main(){
+    cout << "Jumlah Mahasiswa : " << mahasiswa::getJumlahMahasiswa() << endl;
+    mahasiswa mhs1("Andi");
+    mahasiswa mhs2("Bila");
+    mahasiswa::setJumlah(5);
+    {
+        mahasiswa mhs3("Dizza");
+        cout << "Jumlah Mahasiswa : " << mahasiswa::getJumlahMahasiswa() << endl;
+    }
+
+    cout << "Jumlah Mahasiswa Adalah " << mahasiswa::getJumlahMahasiswa() << endl;
+    return 0;
 };
